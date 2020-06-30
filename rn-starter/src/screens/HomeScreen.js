d752b4b7cbaac60a9199be575e19d911bc6bc0ca
kyantikov <yantikovkirill@gmail.com>
2020-06-30 18:02:26 -0500

    add "Go To List Demo" button via TouchableOpacity; implement navigation between screens

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
       <Button
         title="Go to Components Demo"
-        onPress={ () => {
-          console.log('button pressed');
-        }}
-      />
+        onPress={ () =>  props.navigation.navigate('Components') }/>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('List')}>
+        <Text style={styles.simpleButton}>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -18,7 +19,10 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
-  }
+  },
+  simpleButton: {
+    marginVertical: 20
+  },
 });
 
 export default HomeScreen;
