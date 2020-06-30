edf04885352e9a6f2614f8d2b846f9539760e5c9
kyantikov <yantikovkirill@gmail.com>
2020-06-30 12:13:59 -0500

    list/demonstrate the rules of JSX

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen.</Text>
+  // rules of jsx:
+    // can assemble different JSX elements like normal HTML
+    // can configure different JSX elements on the fly using 'props' (=properties)
+      // props will be used to customize our own components that we create
+      // imp: props = @inputs
+    // can refer to JavaScript variable inside of a JSX block using curly braces
+      // variables may reference strings, numbers, arrays of both but NOT JS objects (will receive error)
+    // can assign JSX elements to a variable, then show that variable inside of a JSX block
+      // allows to create JSX conditionally, ahead-of-time (AoT)
+
+  const name = 'Fred';
+  const anotherElement = <Text style={styles.textStyle}>This text element exists as a variable.</Text>
+  return (
+    <View>
+      <Text style={styles.headerTextStyle}>This is the components screen.</Text>
+      <Text>Hello {name}!</Text>
+      {anotherElement}
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
+  headerTextStyle: {
     fontSize: 30
+  },
+  textStyle: {
+    fontSize: 22
   }
 });
 
