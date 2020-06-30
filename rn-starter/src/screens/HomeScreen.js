2b679b48aac62ae9c49627e6cb1cffa7f73321f5
kyantikov <yantikovkirill@gmail.com>
2020-06-30 17:29:15 -0500

    use simple Button to console.log a message; change entry point to Home

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <Text style={styles.text}>Hello World!</Text>
+    <View>
+      <Text style={styles.text}>Hello World!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={ () => {
+          console.log('button pressed');
+        }}
+      />
+    </View>
   );
 };
 
