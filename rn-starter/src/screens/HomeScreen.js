4c622cc24d540a5f04fe394c64f87fe4a21fb0e1
kyantikov <yantikovkirill@gmail.com>
2020-06-30 23:35:24 -0500

    add ImageScreen

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -15,6 +15,10 @@ const HomeScreen = ({ navigation }) => {
       title="Go to List Demo"
       onPress={ () =>  navigation.navigate('List') }
       />
+      <Button
+      title="Go to Image Components Demo"
+      onPress={ () =>  navigation.navigate('Image') }
+      />
     </View>
   );
 };
