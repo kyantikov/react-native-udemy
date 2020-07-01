607e3a20520087044c551aedd17afe8ea1a3cb30
kyantikov <yantikovkirill@gmail.com>
2020-06-30 23:52:49 -0500

    create ImageDetail reusable component, use in ImageScreen

---
+++ rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+// this component is the child in reference to ImageScreen
+const ImageDetail = () => {
+  return <Text>Image Detail</Text>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageDetail;
--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import ImageDetail from "../components/ImageDetail";
 
+// this screen is the parent of ImageDetail (reusable component)
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>
+  return (
+    <View>
+      <ImageDetail/>
+      <ImageDetail/>
+      <ImageDetail/>
+      <ImageDetail/> 
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
