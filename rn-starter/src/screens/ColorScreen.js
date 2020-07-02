476fb3bceeee1c6f200adeeaedd7dff2cba100e2
kyantikov <yantikovkirill@gmail.com>
2020-07-02 14:06:16 -0500

    add randomRgb function; create colors state, update onPress of "Add a Color" button

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -2,10 +2,25 @@ import React, { useState } from "react";
 import { View, StyleSheet, FlatList, Button } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
+  const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+
+    return `rgb(${red},${green},${blue})`;
+  };
 
   return (
     <View>
-      <Button title="Add a Color" />
+      <Button
+        title="Add a Color"
+        onPress={ () => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }}></View>
     </View>
   );
 };
