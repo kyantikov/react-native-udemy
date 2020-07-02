844028d83500992571500375b12895182bda61ba
kyantikov <yantikovkirill@gmail.com>
2020-07-02 15:00:26 -0500

    implement ColorCounter component; consume in SquareScreen

---
+++ rn-starter/src/components/ColorCounter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Button, StyleSheet, Text, View} from "react-native";
+
+const ColorCounter = ({ color }) => {
+  return (
+    <View style={ styles.container }>
+      <Text style={{ color: color.toLowerCase(), ...styles.buttonGroupLabel }}>{ color }</Text>
+      <Button title={`Increase ${color}`} onPress={ () => {}} />
+      <Button title={`Decrease ${color}`} onPress={ () => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 15,
+  },
+  buttonGroupLabel: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default ColorCounter;
