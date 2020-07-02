2f7d2b70112bf35c930682ddb9f50401e4d0c6e3
kyantikov <yantikovkirill@gmail.com>
2020-07-02 12:32:43 -0500

    add ColorScreen

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -6,6 +6,7 @@ import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
 
 
 // props: system to pass data from the parent to a child
@@ -19,6 +20,7 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
+    Color: ColorScreen,
   },
   {
     initialRouteName: "Home",
---
+++ rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { View, StyleSheet, FlatList, Button } from "react-native";
+
+const ColorScreen = () => {
+
+  return (
+    <View>
+      <Button title="Add a Color" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ColorScreen;
