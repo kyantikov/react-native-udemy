ec52db432a4ea46e95094300db9c38cfb303e107
kyantikov <yantikovkirill@gmail.com>
2020-07-02 14:45:33 -0500

    add SquareScreen

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -7,6 +7,7 @@ import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
 import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SquareScreen";
 
 
 // props: system to pass data from the parent to a child
@@ -21,6 +22,7 @@ const navigator = createStackNavigator(
     Image: ImageScreen,
     Counter: CounterScreen,
     Color: ColorScreen,
+    Square: SquareScreen,
   },
   {
     initialRouteName: "Home",
---
+++ rn-starter/src/screens/SquareScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+const SquareScreen = () => {
+  return (
+    <View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SquareScreen;
