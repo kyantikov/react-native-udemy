30b58d244e1d38a9255ad38f7f2c3ee89dfaf0ba
kyantikov <yantikovkirill@gmail.com>
2020-07-03 17:25:47 -0500

    make application functional by using dispatch to trigger useReducer; add const; notes

--- rn-starter/src/screens/ReducerSquareScreen.js
+++ rn-starter/src/screens/ReducerSquareScreen.js
@@ -8,7 +8,7 @@ import ColorCounter from "../components/ColorCounter";
   // state ::: object that has all of our state in it ---> { red: number, green: number, blue: number }
   // action ::: object that describes the update we want to make ---> { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
 const reducer = (state, action) => {
-  switch(action.ColorToChange) {
+  switch(action.colorToChange) {
     case 'red':
       // never modify state directly
       return { ...state, red: state.red + action.amount };
@@ -21,6 +21,8 @@ const reducer = (state, action) => {
   }
 };
 
+const COLOR_INCREMENT = 15;
+
 // how this compares to using the useState hook:
   // REDUCER: accepts object of related states and returns object that contains updated state values
   // SETTER: accepts single state variable and returns its updated value
@@ -28,26 +30,31 @@ const reducer = (state, action) => {
 const ReducerSquareScreen = () => {
 
   // state variable will initially be equal to the initializer object
+  // when useReducer is called, it returns two things:
+    // state: current state
+    // dispatch (aka runMyReducer): invokes reducer and accepts object that describes how we want to change our state -- 'action' argument in reducer function
+  // much like when using the setter of useState hook, useReducer will also rerender the screen when the state updates as a result of our reducer function being called
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+  const { red, green, blue } = state;
 
   return (
     <View>
       <ColorCounter
-        onIncrease={ () => {} }
-        onDecrease={ () => {} }
+        onIncrease={ () => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT }) }
+        onDecrease={ () => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT }) }
         color="Red"
       />
       <ColorCounter
         color="Green"
-        onIncrease={ () => {} }
-        onDecrease={ () => {} }
+        onIncrease={ () => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT }) }
+        onDecrease={ () => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT }) }
       />
       <ColorCounter
         color="Blue"
-        onIncrease={ () => {} }
-        onDecrease={ () => {} }
+        onIncrease={ () => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT }) }
+        onDecrease={ () => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT }) }
       />
-      {/*<View style={{height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}/>*/}
+      <View style={{height: 200, width: 200, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}/>
     </View>
   );
 };
