b18f0d2c770359e2b8b4d4e91ac02ac55d691d59
kyantikov <yantikovkirill@gmail.com>
2020-07-03 20:02:39 -0500

    configure ReducerCounter component to consume useReducer hook; same functionality as CounterScreen

--- rn-starter/src/screens/ReducerCounterScreen.js
+++ rn-starter/src/screens/ReducerCounterScreen.js
@@ -1,21 +1,34 @@
 import React, { useReducer } from "react";
 import { View, StyleSheet, Button, Text } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase_count':
+      return { ...state, count: state.count + action.payload };
+    case 'decrease_count':
+      return { ...state, count: state.count + action.payload };
+    default:
+      return state;
+  }
+};
+
+const INCREMENTER = 1;
 
 const ReducerCounterScreen = () => {
 
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
-        onPress={ () => {
-      }}/>
+        onPress={ () => { dispatch({ type: 'increase_count', payload: INCREMENTER }) }}
+      />
       <Button
         title="Decrease"
-        onPress={ () => {} }
+        onPress={ () => { dispatch({ type: 'decrease_count', payload: -1 * INCREMENTER }) }}
       />
-      <Text>Counter: {}</Text>
+      <Text>Counter: { state.count }</Text>
     </View>
   );
 };
