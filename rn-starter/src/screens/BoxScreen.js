68f905dbbbf826c12c8c19c2f1bac017461d0ab0
kyantikov <yantikovkirill@gmail.com>
2020-07-06 16:44:05 -0500

    use Box Object Model methods to position single element

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
+// Box Object Model refers to height/width of an element + the space around it
+  // use this to affect the positioning of a single element
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'green'
+  },
+  textStyle: {
+    borderWidth: 10,
+    borderColor: 'red',
+    margin: 20,
+    padding: 10,
+  }
 });
 
 export default BoxScreen;
