3b53edb02bd1e1e3d8679f2a975b93830d9ddb8f
kyantikov <yantikovkirill@gmail.com>
2020-07-06 12:53:33 -0500

    setup TextInput to use state within TextScreen; improve notes

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, TextInput } from "react-native";
 
 const TextScreen = () => {
+
+  const [name, setName] = useState('Karl');
+
+  // the TextInput shown here will essentially be the base of mose TextInput fields that are created for your projects
   return (
     <View>
-      {/* - by default, iOS will auto-correct and auto-capitalize the TextInput field.
-            - sometimes, this behavior is undesired, such as in the case of an email/username/password field
-            - there are two props that prevent iOS TextInput fields from having this behavior
-              - autoCapitalize: 'none' | 'sentences' | 'words' | 'characters'
-              - autoCorrect: boolean  */}
+      <Text style={{ fontSize: 25, textAlign: 'center' }}>Enter Name: </Text>
       <TextInput
         style={ styles.input }
-        autoCapitalize="none"
-        autoCorrect={false}
+        autoCapitalize="none" // iOS applies auto-correct/auto-capitalize by default
+        autoCorrect={false} // these props prevents that behavior from TextInput fields that do not need it: email/username/password
+        value={name}
+        onChangeText={ (inputValue) => setName(inputValue) }
       />
+      <Text style={{ textAlign: 'center' }}>My name is {name}</Text>
     </View>
   );
 };
