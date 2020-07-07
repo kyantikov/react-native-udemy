d30a8a2b8a97446fdd6547f5fc3e4101c309bf06
kyantikov <yantikovkirill@gmail.com>
2020-07-06 19:10:33 -0500

    introduce position, top, bottom, left, right properties for child elements; move note from js file -> styleNotes.txt

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -33,8 +33,7 @@ const BoxScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  // Box Object Model refers to height/width of a single element + the space around it
-    // use this to affect the positioning of a single element
+  // see styleNotes.txt for box object model, flex box, and position notes
   container: {
     borderWidth: 3,
     borderColor: 'black'
@@ -56,77 +55,40 @@ const styles = StyleSheet.create({
     margin: 20,
     padding: 10,
   },
-  // Flex Box refers to how some number of sibling elements get laid out inside a parent
-    // use this to position multiple elements with a common parent
-    // there are certain properties which affect how siblings are positioned in a common parent:
-
-      // alignItems ::: affects how children are situated horizontally
-        // property set on PARENT element
-        // default: 'stretch' (children fill up as much HORIZONTAL space as possible)
-        // 'flex-start' | 'center' | 'flex-end'
-        // primary axis changes depending on flexDirection property
-
-      // flexDirection ::: determines whether children are laid out vertically or horizontally ('column', 'row')
-        // property set on PARENT element
-        // default: 'column' (will fill up as much VERTICAL space as possible)
-        // imp! flexDirection affects behavior of alignItems AND justifyContent --- changes primary and secondary axis
-          // alignItems
-            // 'column': works on horizontal axis
-            // 'row': works on vertical axis
-          // justifyContent
-            // 'column': works on vertical axis
-            // 'row': works on horizontal axis
-
-      // justifyContent ::: affects how children are situated vertically
-        // property set on PARENT element
-        // default: 'flex-start' (children are pushed up to top of container)
-        // 'center' | 'flex-end' | 'space-between' | 'space-around'
-        // primary axis changes depending on flexDirection property
-
-      // flex ::: makes a child in parent try take up as much space as possible (depending on the set flexDirection)
-        // property set on a CHILD element
-        // integer values
-        // multiple children inside of one parent can have the flex property assigned
-        // flex can be assigned values greater than 1
-          // this will proportionately size the child components within the parent
-            // 4 + 4 + 2 = 10 'sections'
-            // (imagine that parent gets split into ten equal sections)
-              // flexOne, flexTwo each get 40% of available space, while flexThree gets 20% of available space'
-
-      // alignSelf ::: essentially overrides the alignItems property on the parent element
-        // property set on CHILD element
-        // 'auto' | 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline'
-
 
   flexColumnView: {
-    // alignItems: 'center',
+    alignItems: 'center',
     // justifyContent: 'flex-start',
     height: 150,
   },
   flexRowView: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    // alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     height: 150,
   },
   flexOneText: {
     borderWidth: 3,
     borderColor: 'red',
     // flex: 4,
-    alignSelf: 'center',
+    // alignSelf: 'center',
   },
   flexTwoText: {
     borderWidth: 3,
     borderColor: 'red',
     // flex: 4,
-    alignSelf: 'stretch',
+    // position: 'absolute',
+    top: 23,
+    left: 60,
+
   },
   flexThreeText: {
     borderWidth: 3,
     borderColor: 'red',
     // flex: 2,
-    alignSelf: 'center',
+    // alignSelf: 'center',
   }
+
 });
 
 export default BoxScreen;
