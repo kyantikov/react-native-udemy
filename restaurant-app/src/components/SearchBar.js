b26812a650924069f1e6b0e51284ec4c63b1ab9a
kyantikov <yantikovkirill@gmail.com>
2020-07-07 14:06:38 -0500

    implement SearchBar w/ icon, styling

--- restaurant-app/src/components/SearchBar.js
+++ restaurant-app/src/components/SearchBar.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
+
+import { Ionicons } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View>
-
+    <View style={styles.background}>
+      <View>
+        <Ionicons name="ios-search" size={30} color="black" />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#e8e6e6',
+    height: 45,
+    width: 'auto',
+    borderRadius: 5,
+  },
 
+  searchInput: {
+
+  },
 });
 
 export default SearchBar;
--- restaurant-app/src/screens/SearchScreen.js
+++ restaurant-app/src/screens/SearchScreen.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
+import SearchBar from "../components/SearchBar";
+
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.container}>
+      <SearchBar/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    padding: 10,
+  },
 });
 
 export default SearchScreen;
