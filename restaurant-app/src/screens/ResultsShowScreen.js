38703df9d0875f9deccbd638c63a346534a5d81e
kyantikov <yantikovkirill@gmail.com>
2020-07-07 23:49:56 -0500

    add ResultsShowScreen

---
+++ restaurant-app/src/screens/ResultsShowScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View ,StyleSheet, Text} from "react-native";
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text>Results Show Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ResultsShowScreen;
