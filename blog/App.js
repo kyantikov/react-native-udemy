97a2a47b80b35ef3431c1822e3f8b52852bd078a
kyantikov <yantikovkirill@gmail.com>
2020-08-04 18:53:08 -0500

    refactor navigation related files to follow React-Nav 5.x paradigm

--- blog/App.js
+++ blog/App.js
@@ -1,36 +1,19 @@
 import React from "react";
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
+import { NavigationContainer } from "@react-navigation/native";
 
 import { Provider } from "./src/context/BlogContext";
-import IndexScreen from "./src/screens/IndexScreen";
-import CreateScreen from "./src/screens/CreateScreen";
-import ShowScreen from "./src/screens/ShowScreen";
-import EditScreen from "./src/screens/EditScreen";
-
-const navigator = createStackNavigator({
-  Index: IndexScreen,
-  Show: ShowScreen,
-  Create: CreateScreen,
-  Edit: EditScreen,
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
-  }
-  }
-);
-
-const App = createAppContainer(navigator);
+import { BlogStack } from "./src/navigation/BlogStack";
 
 // Context API solves the problem of needing to pass data down to a nested child component
   // saves having to pass data directly through each child
   // more complicated to set up
 
-export default () => {
+export default App = () => {
   return (
     <Provider>
-      <App />
+      <NavigationContainer>
+        <BlogStack />
+      </NavigationContainer>
     </Provider>
     );
 }
---
+++ blog/src/navigation/BlogStack.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import IndexScreen from "../screens/IndexScreen";
+import CreateScreen from "../screens/CreateScreen";
+import ShowScreen from "../screens/ShowScreen";
+import EditScreen from "../screens/EditScreen";
+
+const Stack = createStackNavigator();
+
+export const BlogStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Index"
+    >
+      <Stack.Screen
+        name="Index"
+        component={IndexScreen}
+      />
+      <Stack.Screen
+        name="Create"
+        component={CreateScreen}
+      />
+      <Stack.Screen
+        name="Show"
+        component={ShowScreen}
+      />
+      <Stack.Screen
+        name="Edit"
+        component={EditScreen}
+      />
+    </Stack.Navigator>
+  )
+};
